323d880468910cdf5ee57c5b655df4a7f1e2c240
ANNA YANG <yanganna68@gmail.com>
2017-12-20 21:44:58 +0000

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,28 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(array)
+  array.each_with_index do |name, index|
+    puts "#{index+1} #{name}"
+  end
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(calls)
+  calls.collect do |call|
+    call.capitalize << "!"
+  end
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(calls)
+  calls.any? do |call|
+    call.size > 4
+  end
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
+def find_the_cheese(array)
   cheese_types = ["cheddar", "gouda", "camembert"]
+  if array.include?("cheddar" || "gouda" || "camembert")
+    array.find do |item|
+      item == "cheddar" || item == "gouda" || item == "camembert"
+    end
+  else
+    nil
+  end
 end
